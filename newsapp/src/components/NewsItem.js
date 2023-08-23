6ca95ffc27b6eab1841be768c0d17e2b983394ec
Saubhagyasingh-shadow <140811958+Saubhagyasingh-shadow@users.noreply.github.com>
2023-08-23 23:22:08 +0530

    final commit

---
+++ newsapp/src/components/NewsItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+   
+  render() {
+    let {title,description,imageUrl,newsUrl,author,date,source}=this.props;
+    return (
+    <div>
+    <div className="card">
+    <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+    {source}
+    </span>
+   <img src={!imageUrl?"https://images.livemint.com/img/2023/02/08/600x338/adani_power_1675846615471_1675846615609_1675846615609.jpg":imageUrl} className="card-img-top" alt="..."/>
+   <div className="card-body">
+    <h5 className="card-title">{title}...
+   </h5>
+    <p className="card-text">{description}...</p>
+    <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
